83bcc241c34df5841317349271f73bcfd700ae73
travisjungroth <jungroth@gmail.com>
2021-04-18 20:20:06 -0500

    Inline check_projects.py

--- check_projects.py
+++ check_projects.py
@@ -1,19 +1,13 @@
 from hecksum.db_models import Project
 
-
-def main():
-    projects = [
-        Project(airtable_id='rec1stqERwHeVoyTr', name='Codecov Bash Uploader'),
-        Project(airtable_id='recPGEEzOeJ2gNh7u', name='Transmission Mac OS X'),
-        Project(airtable_id='rec6xk5CUPcjsqIyD', name='Transmission Windows x86'),
-        Project(airtable_id='recZOMQpGtd524lsj', name='Transmission Windows x64'),
-        Project(airtable_id='recVSRZVqVDt2SCom', name='Transmission Linux'),
-    ]
-    for project in projects:
-        check = project.check()
-        print(check)
-        check.post()
-
-
-if __name__ == '__main__':
-    main()
+projects = [
+    Project(airtable_id='rec1stqERwHeVoyTr', name='Codecov Bash Uploader'),
+    Project(airtable_id='recPGEEzOeJ2gNh7u', name='Transmission Mac OS X'),
+    Project(airtable_id='rec6xk5CUPcjsqIyD', name='Transmission Windows x86'),
+    Project(airtable_id='recZOMQpGtd524lsj', name='Transmission Windows x64'),
+    Project(airtable_id='recVSRZVqVDt2SCom', name='Transmission Linux'),
+]
+for project in projects:
+    check = project.check()
+    print(check)
+    check.post()
